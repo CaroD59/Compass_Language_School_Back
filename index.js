28e9b5accd8d8315fa67c46e7c7e7e1cfe517887
CaroD59 <dorchies.c@gmail.com>
2021-07-19 10:34:49 +0200

    Authentification, contact form and users operational

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const { backPort, frontendUrl } = require('./conf');
 
 const app = express();
+const usersRoutes = require('./routes/users');
+const contactRoutes = require('./routes/contactform');
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
+app.use(express.json());
+app.use(passport.initialize());
+app.use(cors({ credentials: true, origin: frontendUrl }));
+app.use('/users', usersRoutes);
+app.use('/contact', contactRoutes);
+
+app.use('/auth', require('./routes/auth'));
+
+app.use((req, res) => {
+  const msg = `Page not found: ${req.url}`;
+  console.warn(msg);
+  res.status(404).send(msg);
 });
 
-app.use('/', (req, res) => {
-  res.status(404).send('Route not found! ');
-});
-
-app.listen(5050, () => {
-  console.log('Terra Battle API now available on http://localhost:5050 !');
+app.listen(backPort, () => {
+  console.log(`API root available at: http://localhost:${backPort}/`);
 });
